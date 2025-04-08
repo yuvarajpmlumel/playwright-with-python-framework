3122f76ffe5ad95621d612ba8f5d962689505783
yuvaraj <yuvarajp@lumel.com>
2025-04-08 14:40:23 +0530

    added remove warning

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,6 +6,8 @@ import pytest
 def before_each(launch_url):
     launch_url(appURL.SANITY)
 
+@pytest.mark.sanity
+@pytest.mark.abc
 def test_search(page):
     searchbar = Search(page)
     # Navigate to the website
@@ -13,6 +15,17 @@ def test_search(page):
     searchbar.check_search_bar_displayed()
 
     searchbar.click_search()
+    searchbar.check_search_bar_not_displayed()
+
+    print("All tests passed!")
+
+def xtest_search1(page):
+    searchbar = Search(page)
+    # Navigate to the website
+    searchbar.click_search()
+    searchbar.check_search_bar_displayed()
+
+    searchbar.click_search1()
     searchbar.check_search_bar_not_displayed()
 
     print("All tests passed!")
